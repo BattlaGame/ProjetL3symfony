222e8e987b46904d76e3cca0323614f01c28a621
Alexandre-Chuzel <alexandre.chuzel@gmail.com>
2022-11-03 13:09:14 +0100

    edit profile

--- src/Entity/Adherent.php
+++ src/Entity/Adherent.php
@@ -65,4 +65,5 @@ class Adherent
 
         return $this;
     }
+    
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,17 +6,23 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Vich\UploaderBundle\Form\Type\VichImageType;
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email')
-            ->add('roles')
+            /*->add('roles')*/
+            ->add('imageFile', VichImageType::class, array(
+                'required' => false,
+                'allow_delete' => true, // not mandatory, default is true
+                'download_link' => true, // not mandatory, default is true
+                ))
             ->add('name')
             ->add('surname')
             ->add('dateDeNaissance')
+
             ->add('password')
             ->add('adherent')
         ;
