e96185f6e316da880054c6bf69cfcb586e987277
Corentin Maury <you@example.com>
2022-11-12 14:16:00 +0100

    petit detail

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Email déjà utiliser')]
 #[Vich\Uploadable]
 class User implements UserInterface, PasswordAuthenticatedUserInterface, \Serializable
 {
